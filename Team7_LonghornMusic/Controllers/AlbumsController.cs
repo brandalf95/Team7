861c4a686553946457b9c884d8eb2dcc104744b7
brandalf95 <brandonrodriguez@utexas.edu>
2016-11-18 17:53:02 -0600

    Album Search Pretty Much Done

--- Team7_LonghornMusic/Controllers/AlbumsController.cs
+++ Team7_LonghornMusic/Controllers/AlbumsController.cs
@@ -17,11 +17,12 @@ namespace Team7_LonghornMusic.Controllers
         // GET: Albums
         public ActionResult Index(String SearchString)
         {
-            var averageRating =
-              (from r in db.AlbumReviews
-               select r.Rating).Average();
+            //TODO: FIGURE THIS OUT
+            //var averageRating =
+              //(from r in db.AlbumReviews
+               //select r.Rating).Average();
 
-            ViewBag.avgRating = averageRating;
+            //ViewBag.avgRating = averageRating;
 
             List<Album> SelectedAlbums = new List<Album>();
             List<Album> TotalAlbums = new List<Album>();
@@ -41,9 +42,123 @@ namespace Team7_LonghornMusic.Controllers
 
             SelectedAlbums = SelectedAlbums.OrderBy(a => a.AlbumTitle).ToList();
             return View(SelectedAlbums);
-
         }
 
+        public ActionResult DetailedSearch()
+        {
+            ViewBag.AllGenres = GetAllGenres();
+            ViewBag.AllArtists = GetAllArtists();
+            return View();
+        }
+
+        public ActionResult SearchResults(String SearchString, int[] SelectedGenres, int[] SelectedArtists)
+        {
+            var query = from a in db.Albums
+                        select a;
+
+
+            //code for textbox
+            if (SearchString == null || SearchString == "")
+            {
+                query = query.Where(a => a.AlbumTitle != null);
+            }
+            else
+            {
+                query = query.Where(a => a.AlbumTitle.Contains(SearchString));
+            }
+
+            List<Album> DisplayAlbums = new List<Album>();
+
+            //code for artist filter
+            if (SelectedArtists != null)
+            {
+                foreach (int i in SelectedArtists)
+                {
+                    List<Album> AlbumsFound = query.Where(a => a.AlbumArtists.Any(ar => ar.ArtistID == i)).ToList();
+
+                    foreach (Album a in AlbumsFound)
+                    {
+                        DisplayAlbums.Add(a);
+                    }
+                }
+            }
+            else
+            {
+                List<Album> AlbumsFound = query.Where(a => a.AlbumArtists.Any()).ToList();
+
+                foreach (Album a in AlbumsFound)
+                {
+                    DisplayAlbums.Add(a);
+                }
+            }
+
+            //code for genre filter
+            if (SelectedGenres != null)
+            {
+                foreach (int i in SelectedGenres)
+                {
+                    List<Album> AlbumsFound = query.Where(a => a.AlbumGenres.Any(g => g.GenreID == i)).ToList();
+
+                    foreach (Album a in AlbumsFound)
+                    {
+                        DisplayAlbums.Add(a);
+                    }
+                }
+            }
+            else
+            {
+                List<Album> AlbumsFound = query.Where(a => a.AlbumGenres.Any()).ToList();
+
+                foreach (Album a in AlbumsFound)
+                {
+                    DisplayAlbums.Add(a);
+                }
+            }
+
+            //TODO: code for Rating Filter for Album
+            // if 
+
+            List<Album> SelectedAlbums = DisplayAlbums.Distinct().ToList();
+
+            var TotalAlbums = db.Albums.ToList();
+            ViewBag.SelectedAlbumCount = "Displaying " + SelectedAlbums.Count() + " of " + TotalAlbums.Count() + " Records";
+
+            SelectedAlbums = SelectedAlbums.OrderBy(a => a.AlbumTitle).ToList();
+
+            return View("Index", SelectedAlbums);
+        }
+
+        public MultiSelectList GetAllGenres()
+        {
+            var query = from c in db.Genres
+                        orderby c.GenreName
+                        select c;
+
+            List<Genre> allGenres = query.ToList();
+
+            //Add in choice for not selecting a frequency
+            //Genre NoChoice = new Genre() { GenreID = 0, GenreName = "All" };
+            //allGenres.Add(NoChoice);
+            MultiSelectList GenreList = new MultiSelectList(allGenres.OrderBy(g => g.GenreName), "GenreID", "GenreName");
+            return GenreList;
+        }
+
+        public MultiSelectList GetAllArtists()
+        {
+            var query = from c in db.Artists
+                        orderby c.ArtistName
+                        select c;
+
+            List<Artist> allArtists = query.ToList();
+
+            //Add in choice for not selecting a frequency
+            //Genre NoChoice = new Genre() { GenreID = 0, GenreName = "All" };
+            //allGenres.Add(NoChoice);
+            MultiSelectList ArtistList = new MultiSelectList(allArtists.OrderBy(a => a.ArtistName), "ArtistID", "ArtistName");
+            return ArtistList;
+        }
+
+        
         // GET: Albums/Details/5
         public ActionResult Details(int? id)
         {
